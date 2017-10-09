578a53c1407f619bffa835de812a958d6dae0cea
Logan McAnsh <logan@mcan.sh>
2017-10-08 23:58:24 -0400

    help with inital unstyled load

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
 
 class Page extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+    const styles = flush();
+    return { html, head, errorHtml, chunks, styles };
+  }
   render() {
     return (
       <html lang="en">
