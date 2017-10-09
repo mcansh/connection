adc3419c4becf3ee30cc3018354cef7435a3ea82
Logan McAnsh <logan@mcan.sh>
2017-10-09 00:13:09 -0400

    icons, manifest for service worker

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,55 @@ class Page extends Document {
             name="viewport"
             content="initial-scale=1.0, width=device-width, viewport-fit=cover"
           />
+          <link
+            rel="shortcut icon"
+            href="/static/favicon-32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="228x228"
+            href="/static/favicon-228.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="195x195"
+            href="/static/favicon-195.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href="/static/favicon-152.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/static/favicon-144.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="128x128"
+            href="/static/favicon-128.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href="/static/favicon-120.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="96x96"
+            href="/static/favicon-96.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href="/static/favicon-72.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            href="/static/favicon-57.png"
+          />
           <meta
             name="description"
             content="Get your computers network speed 🚀"
