673753b6dc40a96f96a0fe4459f6d9353a5dbe5f
Logan McAnsh <logan@mcan.sh>
2017-10-09 00:44:47 -0400

    for whatever reason, having this the other way prevented the event from firing

--- pages/index.js
+++ pages/index.js
@@ -18,51 +18,49 @@ class Index extends React.Component {
         .then(console.log('service worker registration successful'))
         .catch(err => console.warn(err));
     }
-    if ('connection' in navigator) {
-      this.updateConnectionInfo();
-      navigator.connection.addEventListener(
-        'change',
-        this.updateConnectionInfo,
-      );
-    } else {
-      const error = "Your browser doesn't support navigator.connection 🙃";
-      this.setState({ error });
-    }
-  }
+    const updateConnectionInfo = () => {
+      if ('connection' in navigator) {
+        const network = navigator.connection;
 
-  updateConnectionInfo() {
-    const network = navigator.connection;
+        const { type, downlink, rtt } = network;
 
-    const { type, downlink, rtt } = network;
+        console.log(
+          `MDN says navigator.connection.downlink is in mbps but it looks like its closer to MBps? Take a speedtest and compare and lemme know. I got ${downlink}, where in mbps thats ${downlink *
+            8}`,
+        );
 
-    console.log(
-      `MDN says navigator.connection.downlink is in mbps but it looks like its closer to MBps? Take a speedtest and compare and lemme know. I got ${downlink}, where in mbps thats ${downlink *
-        8}`,
-    );
+        const connections = [
+          { type: 'wifi', name: 'WiFi' },
+          { type: 'bluetooth', name: 'Bluetooth' },
+          { type: 'cellular', name: 'Cellular' },
+          { type: 'wimax', name: 'WiMAX' },
+          { type: 'ethernet', name: 'Ethernet' },
+          { type: 'other', name: 'magic' },
+        ];
 
-    const connections = [
-      { type: 'wifi', name: 'WiFi' },
-      { type: 'bluetooth', name: 'Bluetooth' },
-      { type: 'cellular', name: 'Cellular' },
-      { type: 'wimax', name: 'WiMAX' },
-      { type: 'ethernet', name: 'Ethernet' },
-      { type: 'other', name: 'magic' },
-    ];
+        const connection = connections.find(data => type === data.type);
 
-    const connection = connections.find(data => type === data.type);
+        let connectionSentence = '';
+        if (type === 'unknown') {
+          connectionSentence = "I have no idea how you're online";
+        } else if (type === 'none') {
+          connectionSentence = 'You are not connected to the internet';
+        } else {
+          connectionSentence = `You are connected via ${connection.name}`;
+        }
 
-    let connectionSentence = '';
-    if (type === 'unknown') {
-      connectionSentence = "I have no idea how you're online";
-    } else if (type === 'none') {
-      connectionSentence = 'You are not connected to the internet';
-    } else {
-      connectionSentence = `You are connected via ${connection.name}`;
-    }
+        const bandwidthSentence =
+          type !== 'none' ? `Your bandwidth is ${downlink}mbps` : '';
+        const rttSentence = type !== 'none' ? `Your ping is ${rtt}ms` : '';
+        this.setState({ connectionSentence, bandwidthSentence, rttSentence });
+        navigator.connection.addEventListener('change', updateConnectionInfo);
+      } else {
+        const error = "Your browser doesn't support navigator.connection 🙃";
+        this.setState({ error });
+      }
+    };
 
-    const bandwidthSentence = `Your bandwidth is ${downlink}mbps`;
-    const rttSentence = `Your ping is ${rtt}ms`;
-    this.setState({ connectionSentence, bandwidthSentence, rttSentence });
+    updateConnectionInfo();
   }
 
   render() {
