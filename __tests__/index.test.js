ada6fb4ae63d07d23bcf4d88e1eb7daff718d4f9
Logan McAnsh <logan@mcan.sh>
2019-07-28 15:58:05 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,6 @@
-/* eslint-env jest */
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Index from '../pages/index';
-
-afterEach(cleanup);
 
 describe('<Index />', () => {
   it('The content is rendered', () => {
